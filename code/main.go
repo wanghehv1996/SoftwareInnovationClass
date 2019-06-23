05fb8f42546bce511a0fc33fefd137828e68f860
Raven Chen <raven1996@foxmail.com>
2019-06-23 17:19:34 +0800

    simple go server

--- code/main.go
+++ code/main.go
@@ -12,33 +12,19 @@ package main
 import (
     "net/http"
     "fmt"
-    "time"
     "html/template"
+    "path/filepath"
 )
 
-type Welcome struct {
-    Name string
-    Time string
-}
-
 func main() {
-    welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
-
-    templates := template.Must(template.ParseFiles("templates/index.html"))
-
-    http.Handle("/static/", //final url can be anything
-        http.StripPrefix("/static/",
-            http.FileServer(http.Dir("static")))) 
+    http.Handle("/static/",
+        http.StripPrefix("/static/", http.FileServer(http.Dir("build/static"))))
 
     http.HandleFunc("/" , func(w http.ResponseWriter, r *http.Request) {
-        if name := r.FormValue("name"); name != "" {
-            welcome.Name = name;
-        }
-        if err := templates.ExecuteTemplate(w, "index.html", welcome); err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
+        fp := filepath.Join("build", filepath.Clean(r.URL.Path))
+        tmpl, _ := template.ParseFiles(fp)
+        tmpl.Execute(w, nil)
     })
 
-    fmt.Println("Listening");
     fmt.Println(http.ListenAndServe(":8081", nil));
 }

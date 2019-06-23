6f64401275d8aa3bef9f208bb1b40f55e50884de
havejoy <zouyue1024@163.com>
2019-06-23 13:02:17 +0800

    Front-end production version deployment

---
+++ code/build/precache-manifest.c3efcfcd386f33279a85762bed4ca00b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb90434797b15ba20d86b6f4cf3e2b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f3c72b37dd4baefb7537",
+    "url": "./static/css/2.c0bdfdc0.chunk.css"
+  },
+  {
+    "revision": "a82b56d4467baf286182",
+    "url": "./static/css/main.3134d785.chunk.css"
+  },
+  {
+    "revision": "f3c72b37dd4baefb7537",
+    "url": "./static/js/2.1f475604.chunk.js"
+  },
+  {
+    "revision": "a82b56d4467baf286182",
+    "url": "./static/js/main.31eea804.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8de89256a3b76f473e481f01b5c4843e",
+    "url": "./static/media/logo.8de89256.svg"
+  }
+]);

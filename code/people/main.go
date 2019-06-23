b7a3c572317bdcb78d4da8ed7240f806680382e8
Raven Chen <raven1996@foxmail.com>
2019-06-23 20:21:38 +0800

    people

--- code/people/main.go
+++ code/people/main.go
@@ -3,18 +3,16 @@ package main
 import (
     "net/http"
     "fmt"
-    "html/template"
-    "path/filepath"
 )
 
 func main() {
-    http.Handle("/static/",
-        http.StripPrefix("/static/", http.FileServer(http.Dir("build/static"))))
+    http.Handle("/img/",
+        http.StripPrefix("/img/", http.FileServer(http.Dir("build/static"))))
 
     http.HandleFunc("/" , func(w http.ResponseWriter, r *http.Request) {
-        fp := filepath.Join("build", filepath.Clean(r.URL.Path))
-        tmpl, _ := template.ParseFiles(fp)
-        tmpl.Execute(w, nil)
+        if r.URL.Path == "/people.json" {
+        http.ServeFile(w, r, "people.json")
+        }
     })
 
     fmt.Println(http.ListenAndServe(":8082", nil));
